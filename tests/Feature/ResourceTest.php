7c71eae3743cded4f53cd98348c5a76cf46d0691
dg <d.g.dev482@gmail.com>
2021-03-29 19:38:28 +0300

    fix code style

--- tests/Feature/ResourceTest.php
+++ tests/Feature/ResourceTest.php
@@ -66,44 +66,6 @@ class ResourceTest extends TestCase
 
         $this->assertCount(4, $arResource['columns']);
         $this->assertCount(1, $arResource['actions']);
-
-        var_dump($arResource);
-    }
-}
-
-/**
- * Class TestUser
- * @package Dg482\Red\Tests\Feature
- */
-class TestUser implements Model
-{
-    /** @var int */
-    public int $id;
-
-    /** @var string */
-    public string $email;
-
-    /** @var string */
-    public string $name;
-
-    public function update(array $attributes, array $options = [])
-    {
-        return false;
-    }
-
-    public function getFields(): array
-    {
-        return ['id', 'email', 'name'];
-    }
-
-    public function create(array $request): Model
-    {
-        list($this->id, $this->email, $this->name) = $request;
-        return $this;
-    }
-
-    public function getFillable()
-    {
-        return ['email', 'name'];
+        $this->assertCount(4, $arResource['data']);
     }
 }
