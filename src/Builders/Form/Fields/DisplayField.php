3943126a3abcbf40f6cc16a55818d4d6200bbcf3
Andrey Martynov <a.martinov@ddemo.ru>
2021-04-18 12:43:37 +0500

    add DisplayField.php

---
+++ src/Builders/Form/Fields/DisplayField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dg482\Red\Builders\Form\Fields;
+
+/**
+ * Class DisplayField
+ * @package Dg482\Red\Builders\Form\Fields
+ */
+class DisplayField extends Field
+{
+    /** @var string */
+    const FIELD_TYPE = 'display';
+}
